f8e88c4306f30a8f2e66e058ce6b64bce969336e
CarlosPratesBizinotto <viniciusbizinotto@gmail.com>
2023-12-10 16:43:52 -0300

    ProjetoFinal

---
+++ PrimeiroDesafioLP.js
@@ -0,0 +1,22 @@
+let heroi = "Carlos"
+
+let xp = 99001
+
+
+if(xp <= 1000){
+    console.log("O Herói de nome " + heroi + " esta no nível de Ferro")
+} else if(xp >= 1001 && xp <= 2000 ){
+    console.log("O Herói de nome " + heroi + " esta no nível de Bronze")
+} else if(xp >= 2001 && xp <= 5000 ){
+    console.log("O Herói de nome " + heroi + " esta no nível de Prata")
+} else if(xp >= 6001 && xp <= 7000 ){
+    console.log("O Herói de nome " + heroi + " esta no nível de Ouro")
+} else if(xp >= 7001 && xp <= 8000 ){
+    console.log("O Herói de nome " + heroi + " esta no nível de Platina")
+} else if(xp >= 8001 && xp <= 9000 ){
+    console.log("O Herói de nome " + heroi + " esta no nível de Ascendente")
+} else if(xp >= 9001 && xp <= 10000 ){
+    console.log("O Herói de nome " + heroi + " esta no nível Imortal")
+}else{
+    console.log("O Herói de nome " + heroi + " esta no nível Radiante")
+}
